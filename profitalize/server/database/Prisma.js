1ed52bf39fee71669a71bb9494f2498f5d8fc4cd
ShriyaAl <alladishriya05@gmail.com>
2025-07-11 15:14:28 +0530

    Removed errors

--- profitalize/server/database/Prisma.js
+++ profitalize/server/database/Prisma.js
@@ -1,11 +1,23 @@
-import {PrismaClient} from '@prisma/client';
+// import {PrismaClient} from '@prisma/client';
+
+// const prisma = new PrismaClient();
+
+// // Handle graceful shutdown
+// process.on('SIGTERM', async () => {
+//     await prisma.$disconnect();
+//     process.exit(0);
+//   });
+  
+// export default prisma;
+
+import { PrismaClient } from '../../src/generated/prisma/index.js'; // Explicitly import index.js
 
 const prisma = new PrismaClient();
 
 // Handle graceful shutdown
 process.on('SIGTERM', async () => {
-    await prisma.$disconnect();
-    process.exit(0);
-  });
-  
+  await prisma.$disconnect();
+  process.exit(0);
+});
+
 export default prisma;
